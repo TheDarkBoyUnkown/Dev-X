4bdfb6bb901372a5e86ad324293cc01c4b5c9e52
JakirOnLive <154320286+TheDarkBoyUnkown@users.noreply.github.com>
2025-07-31 23:22:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+clear
+echo "🔧 Installing Dev-X Tool..."
+sleep 1
 
+chmod +x devx.sh
+chmod +x uninstall.sh
+chmod +x update.sh
+chmod +x core/*.sh
+chmod +x tools/*/*.sh
+
+ln -sf "$PWD/devx.sh" /data/data/com.termux/files/usr/bin/devx
+
+echo "✅ Installed successfully!"
+echo "👉 Type 'devx' to launch the tool."
